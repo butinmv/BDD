6cb4d18368ab76687c58cf3c724c44b5d38090cd
Maxim Butin <butinmv9722@gmail.com>
2019-04-11 20:57:10 +0700

    Тесты не работают, но компиляться

---
+++ src/main/java/Product.java
@@ -0,0 +1,12 @@
+public class Product {
+    public Product(int price, String name) {
+    }
+
+    public int getPrice() {
+        return 0;
+    }
+
+    public String getName() {
+        return "";
+    }
+}
