c42a31633c1e46a80340c9ef334dee460ade69e2
Maxim Butin <butinmv9722@gmail.com>
2019-04-11 20:58:38 +0700

    Тесты и работают и компиляться)

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,12 +1,17 @@
 public class Product {
+    private int price;
+    private String name;
+
     public Product(int price, String name) {
+        this.price = price;
+        this.name = name;
     }
 
     public int getPrice() {
-        return 0;
+        return this.price;
     }
 
     public String getName() {
-        return "";
+        return this.name;
     }
 }
